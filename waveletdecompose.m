5c68d6f49ae1f3a67dd959b9bc7674a3bb86c087
Rajesh Tripathy <rajesh.nitr11@gmail.com>
2021-10-11 11:29:56 +0530

    Update waveletdecompose.m

--- waveletdecompose.m
+++ waveletdecompose.m
@@ -1,6 +1,6 @@
 function [xa, xd1, xd2, xd3, xd4, xd5]=waveletdecompose(kk)
 [C,L] = wavedec(kk, 5, 'db4');
-xa= wrcoef('d',C,L,'db4',5);
+xa= wrcoef('a',C,L,'db4',5);
 xd1= wrcoef('d',C,L,'db4',5);
 xd2= wrcoef('d',C,L,'db4',4);
 xd3= wrcoef('d',C,L,'db4',3);
